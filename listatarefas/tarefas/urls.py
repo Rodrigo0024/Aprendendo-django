b94c0387dd56d77048d7e700cbad67514de793b8
rodrigo <24rodrigocurso@gmail.com>
2025-08-19 11:57:00 -0300

    Adicionei a função de remover a tarefa

--- listatarefas/tarefas/urls.py
+++ listatarefas/tarefas/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.lista_tarefas, name='lista_tarefas'),
     path('add/', views.add_tarefas, name='add_tarefas'),
+    path('remover/<int:tarefa_id>/', views.remover_tarefa, name='remover_tarefa'),
 ]
